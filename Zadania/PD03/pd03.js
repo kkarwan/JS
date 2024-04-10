eed07fce5235ef6d826f89cdcd6e3b4d1e4fd17a
Krzysztof Karwan <krzysztof.karwan@hotmail.com>
2024-04-10 17:26:45 +0200

    dodatek

--- Zadania/PD03/pd03.js
+++ Zadania/PD03/pd03.js
@@ -3,4 +3,6 @@ function obrazek(img) {
    fullimg.src = img.src;
    fullimg.alt = img.alt;
    fullimg.setAttribute("title",img.alt);
+   //to jest to samo co wyżej można zapisać tak:
+   //fullimg.title = img.alt;
 }
